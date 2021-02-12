f7a28db40b48c1f62adc747eba4794047f83b0da
Aris Prabowo <aris@hijrstudio.com>
2021-02-12 13:36:27 +0700

    Create component item, style token, update fontawesome

--- resources/views/pelanggan/pages/home.blade.php
+++ resources/views/pelanggan/pages/home.blade.php
@@ -3,4 +3,14 @@
 
 @section('content')
     <x-pelanggan.navbar/>
+    <div class="container" style="margin-top: 120px">
+        <div class="row">
+            @for ($i = 0; $i < 8; $i++)
+                <div class="col-3">
+                    <x-pelanggan.item/>
+                </div>
+            @endfor
+        </div>
+    </div>
+    
 @endsection
